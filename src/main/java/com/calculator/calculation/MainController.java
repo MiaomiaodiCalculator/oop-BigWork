855f5c1b83a018838af2112f1a4a44c80c5b39d4
ZhouYH <2379349903@qq.com>
2023-11-28 18:28:28 +0800

    加了一些单位换算，其中Volume和Length可以正常运行

--- src/main/java/com/calculator/calculation/MainController.java
+++ src/main/java/com/calculator/calculation/MainController.java
@@ -57,6 +57,13 @@ public class MainController implements Initializable {
      **/
     @FXML
     public void equationShift() {loadPage("Equation.fxml");}
+    /**
+     * @Description   加载单位换算页面
+     * @author ZhouYH
+     * @date 2023/11/27 19:37
+     **/
+    @FXML
+    public void UnitTranslation() {loadPage("UnitTranslation.fxml");}
     /***
      * @Description  加载页面的函数
      * @param fxmlFileName 文件名称
@@ -71,5 +78,4 @@ public class MainController implements Initializable {
             System.out.println("error");
         }
     }
-
 }
